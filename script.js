c22b9cde76642e263d57c97aae55fa01b3075502
Elliot Park <elliotpark410@gmail.com>
2022-01-03 00:22:58 -0800

    add questions and answers key

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ const questionResponseElement = document.querySelectorAll('.question-response');
 var timeElement = document.querySelector('.time');
 var secondsLeft = 180;
 
+
+var currentQuestionIndex;
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+
 function startGame() {
     console.log('started game');
     startButton.classList.add('hide');
     questionContainerElement.classList.remove('hide');
+    currentQuestionIndex = 0;
     setTime();
+    setNextQuestion();
 }
 
 
@@ -32,26 +40,50 @@ function setTime() {
 
 
 function setNextQuestion() {
-
+    showQuestion(questions[currentQuestionIndex]);
 }
 
-function selectAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const answerButton = document.createElement('answerButton');
+        answerButton.innerText = answer.text;
+        answerButton.classList.add('button');
+        if (answer.correct) {
+            answerButton.dataset.correct = answer.correct   
+        }
+        startButton.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(answerButton);
+    })
 }
+
+function selectAnswer(e) {
+    
+}
+
+
+
 
 function scoreBoard() {
 
 }
 
-;
+
 const questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'What is 2 + 2?',
         answers: [
             {text: '4', correct: true},
             {text: '22', correct: false},
             {text: '8', correct: false},
             {text: '0', correct: false}
+        ],
+        question: 'What is 4 x 5?',
+        answers: [
+            {text: '9', correct: false},
+            {text: '20', correct: true},
+            {text: '-1', correct: false},
+            {text: '45', correct: false}
         ]
     }
 ]
